13e465e72e9b6600849542baf10f3d417b281856
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-09 15:54:02 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -40,7 +40,6 @@ axios({
       fs.writeFileSync(filePath, entry.getData());
       console.log(`Log saved to: ${filePath}`);
     });
-    
   })
   .catch(error => {
     console.error(`Error: ${error.message}`);
