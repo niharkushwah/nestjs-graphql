e22042ce9f4dfd55c19888574d27b8b397730923
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-10 15:47:37 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -4,7 +4,7 @@ const path = require('path');
 const AdmZip = require('adm-zip');
 const owner = 'niharkushwah';
 const repo = 'nestjs-graphql'; 
-const run_id = 0; 
+const run_id = 0;
 const token = ' '; // Replace with your actual token
 const apiUrl = `https://api.github.com/repos/${owner}/${repo}/actions/runs/${run_id}/logs`;
 // Make the GitHub API request using axios
