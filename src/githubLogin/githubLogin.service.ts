ec002c5bee94b6616e6d952eac65a3a5f041b12c
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-11 16:04:40 +0530

    Update githubLogin.service.ts

--- src/githubLogin/githubLogin.service.ts
+++ src/githubLogin/githubLogin.service.ts
@@ -6,7 +6,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
   
 @Injectable()  
-export class GithubLoginService {
+export class GithubLoginService { 
     constructor(@InjectModel('GitHubUser') private readonly GitHubUserDetails: Model<GitHubUserDetails>) { }
     async githubLogin(): Promise<{ githubAuthUrl: string }> {
         const params = new URLSearchParams();
