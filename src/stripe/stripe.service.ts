372106174a99ed1e5c14edc186aa783e14a52520
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-15 17:47:38 +0530

    Update stripe.service.ts

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 @Injectable()
 export class StripeService {
     private stripe: Stripe;  
-    constructor() {  
+    constructor() {   
         this.stripe = new Stripe('sk_test_51NlTfRSC6eBuCJlad13u1ZHgndEIAiVe7Mw11vGiZ2hHVwslm1OqTiqotGy00TD6DMgtk', {
             apiVersion: '2023-08-16',
         }); 
