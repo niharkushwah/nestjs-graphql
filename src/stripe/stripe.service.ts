1261bc225506b2de060d686c24d524bab1097dba
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-15 14:52:05 +0530

    Update stripe.service.ts

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 export class StripeService {
     private stripe: Stripe;
     constructor() { 
-        this.stripe = new Stripe('sk_test_51NlTfRSC6eBuCJlad13u1ZHgndEIAiVewzpwEHIdcAv8x7Mw11vGiZ2hHVwslm1OqTiqotGy00TD6DMgtk', {
+        this.stripe = new Stripe('sk_test_51NlTfRSC6eBuCJlad13u1ZHgndEIAiVe7Mw11vGiZ2hHVwslm1OqTiqotGy00TD6DMgtk', {
             apiVersion: '2023-08-16',
         });
     }
