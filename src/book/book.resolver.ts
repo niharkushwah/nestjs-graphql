f1f80def26f18ffcc05c710ccd5e1722f915a618
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-01-22 17:51:01 +0530

    Update book.resolver.ts

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Author, Book, BookInput } from './book.schema';
 import { BookService } from './book.service';
-import { Types } from 'mongoose'; 
+import { Types } from 'mongoose';
 @Resolver(() => Book)
 export class BookResolver { 
   constructor(private readonly bookService: BookService) { }
