aeb036341a8886e5f518cdf922389313deec9892
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-06 10:18:25 +0530

    Update book.service.ts

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -4,7 +4,7 @@ import { Model } from "mongoose";
 import { Book, BookInput, NewAuthors } from "./book.schema";
 import { User } from "src/user/user.schema";
 import { InjectModel } from "@nestjs/mongoose";
-import { Author } from "./book.schema"; 
+import { Author } from "./book.schema";
 
 @Injectable()
 export class BookService {
