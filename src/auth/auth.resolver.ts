9c2a6cdf14e1f81f46816efc3bf71d31f5307c1e
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-07 16:37:24 +0530

    Update auth.resolver.ts

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthService } from './auth.service'; 
 
 @Resolver()
-export class AuthResolver { 
+export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => String)
