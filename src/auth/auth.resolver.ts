7284f26c3d1cf89a1ebb55a30a226334575b13af
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-07 16:39:49 +0530

    Update auth.resolver.ts

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql'; 
 import { AuthService } from './auth.service'; 
  
 @Resolver()
