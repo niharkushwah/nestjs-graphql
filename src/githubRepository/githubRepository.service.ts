b15ae89ffc0caf4f55106be05b07425f8a81abdd
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-15 13:06:58 +0530

    Update githubRepository.service.ts

--- src/githubRepository/githubRepository.service.ts
+++ src/githubRepository/githubRepository.service.ts
@@ -1,4 +1,4 @@
-import mongoose, { Model } from 'mongoose'; 
+import mongoose, { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { GitHubRepository, RepositoryType } from './githubRepository.schema';
