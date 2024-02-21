da4deae21a48756a2ffe244ba510e540f399b3f9
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-21 10:13:49 +0530

    Update stripe.module.ts

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'; 
 import { StripeService } from './stripe.service';
 import { StripeResolver } from './stripe.resolver';
 import { StripeController } from './stripe.controller';
