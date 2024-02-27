3a21142e322ed8adb962a5c07d3da2ef64127c13
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-02-27 14:29:54 +0530

    Update githubLogin.schema.ts

--- src/githubLogin/githubLogin.schema.ts
+++ src/githubLogin/githubLogin.schema.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType, Scalar } from '@nestjs/graphql';
+import { Field, ID, ObjectType, Scalar } from '@nestjs/graphql'; 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'; 
 import { Document } from 'mongoose'; 
 import { GraphQLJSONObject } from 'graphql-type-json';
